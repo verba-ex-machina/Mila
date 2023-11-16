2af4c1890be5850797ff3c300f2630b1b47c0ff7
Kristen Steffen <762141+haxys@users.noreply.github.com>
2023-11-16 14:51:21 -0600

    Simplify.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 import os
 
 import discord
-from discord.ext import commands
 
 from lib import Mila
 from lib.constants import DESCRIPTION
@@ -16,27 +15,24 @@ INTENTS = discord.Intents.default()
 INTENTS.members = True
 INTENTS.message_content = True
 
-BOT = commands.Bot(
-    command_prefix=";", description=DESCRIPTION, intents=INTENTS
-)
+
+class MilaBot(discord.Client):
+    """Implement a Discord Bot for Mila."""
+
+    async def on_ready(self):
+        """Print a message when the bot is ready."""
+        print(f"Logged in as {self.user}.")
+
+    async def on_message(self, message):
+        """Respond to messages."""
+        if message.author == self.user:
+            return
+        # Respond to messages that mention the bot.
+        if self.user.mentioned_in(message):
+            await message.channel.send(MILA.prompt(message.content))
 
 
-@BOT.event
-async def on_ready():
-    """Print a message when the bot is ready."""
-    print(f"Logged in as {BOT.user}.")
-
-
-@BOT.command()
-async def status(ctx):
-    """Print the bot's status."""
-    await ctx.send("**Status:** Online")
-
-
-@BOT.command(description="Ask Mila for a joke.")
-async def joke(ctx):
-    """Ask Mila for a joke."""
-    await ctx.send(MILA.prompt("Tell me a joke."))
+BOT = MilaBot(description=DESCRIPTION, intents=INTENTS)
 
 
 def main():
