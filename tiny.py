d85e98bd7db5596ba6121587dc5ec448d345d679
Kristen Steffen <762141+haxys@users.noreply.github.com>
2023-11-20 09:42:39 -0600

    Made Tiny async.

--- tiny.py
+++ tiny.py
@@ -1,99 +1,104 @@
 #!/usr/bin/env python3
 
-from openai import OpenAI
-import time, json
+from openai import AsyncOpenAI
+import time, json, asyncio
 
-def get_horoscope(star_sign: str) -> str:
+async def get_horoscope(star_sign: str) -> str:
     """Get the horoscope for a given star sign."""
     print("Function called: get_horoscope")
     return f"Your horoscope for {star_sign} is: {horoscope_for(star_sign)}"
 
 def horoscope_for(star_sign: str) -> str:
     """Get the horoscope for a given star sign."""
-    return "You will be reminded of your own mortality."
+    return "You are wonderful. Do something nice for yourself today. The stars demand it."
 
-LLM = OpenAI()
-ASSISTANT = LLM.beta.assistants.create(
-    instructions="You are a personal assistant for a busy professional named Kristen. You help them with their daily tasks.",
-    name="Mila",
-    model="gpt-3.5-turbo-16k",
-    tools=[
-        {
-            "type": "function",
-            "function": {
-                "name": "get_horoscope",
-                "description": "Get the horoscope for a given star sign.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "star_sign": {
-                            "type": "string",
-                            "description": "The star sign to get the horoscope for.",
-                        }
-                    },
-                    "required": ["star_sign"]
+async def main():
+    """Launch the Tiny Assistant demo."""
+    llm = AsyncOpenAI()
+    assistant = await llm.beta.assistants.create(
+        instructions="You are a tiny AI assistant with a big heart.",
+        name="Tiny",
+        model="gpt-3.5-turbo-16k",
+        tools=[
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_horoscope",
+                    "description": "Get the horoscope for a given star sign.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "star_sign": {
+                                "type": "string",
+                                "description": "The star sign to get the horoscope for.",
+                            }
+                        },
+                        "required": ["star_sign"]
+                    }
                 }
             }
-        }
-    ]
-)
-THREAD = LLM.beta.threads.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "What's my horoscope for today? I was born February 2nd.",
-        }
-    ]
-)
-RUN = LLM.beta.threads.runs.create(
-    thread_id=THREAD.id,
-    assistant_id=ASSISTANT.id,
-)
-TOOL_OUTPUTS = []
-while True:
-    RUN = LLM.beta.threads.runs.retrieve(
-        thread_id=THREAD.id,
-        run_id=RUN.id,
+        ]
     )
-    if RUN.status == "completed":
-        break
-    elif RUN.status in [
-        "cancelled",
-        "expired",
-        "failed",
-    ]:
-        raise RuntimeError(f"Run failed: {RUN.status}")
-    elif RUN.status == "requires_action":
-        for tool_call in RUN.required_action.submit_tool_outputs.tool_calls:
-            arguments = json.loads(tool_call.function.arguments)
-            name = tool_call.function.name
-            if name == "get_horoscope":
-                response = get_horoscope(**arguments)
-                id = tool_call.id
-                TOOL_OUTPUTS.append(
-                    {
-                        "tool_call_id": id,
-                        "output": response,
-                    }
-                )
-            else:
-                raise RuntimeError(f"Unknown tool call: {name}")
-        RUN = LLM.beta.threads.runs.submit_tool_outputs(
-            thread_id=THREAD.id,
-            run_id=RUN.id,
-            tool_outputs=TOOL_OUTPUTS,
+    thread = await llm.beta.threads.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "What's my horoscope for today? I'm a Capricorn.",
+            }
+        ]
+    )
+    run = await llm.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
+    tool_outputs = []
+    while True:
+        run = await llm.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
         )
-    else:
-        time.sleep(1)
-MESSAGES = LLM.beta.threads.messages.list(
-    thread_id=THREAD.id,
-)
-for message in MESSAGES.data:
-    for content in message.content:
-        print(content.text.value)
-LLM.beta.assistants.delete(
-    assistant_id=ASSISTANT.id,
-)
-LLM.beta.threads.delete(
-    thread_id=THREAD.id,
-)
+        if run.status == "completed":
+            break
+        elif run.status in [
+            "cancelled",
+            "expired",
+            "failed",
+        ]:
+            raise RuntimeError(f"Run failed: {run.status}")
+        elif run.status == "requires_action":
+            for tool_call in run.required_action.submit_tool_outputs.tool_calls:
+                arguments = json.loads(tool_call.function.arguments)
+                name = tool_call.function.name
+                if name == "get_horoscope":
+                    response = await get_horoscope(**arguments)
+                    id = tool_call.id
+                    tool_outputs.append(
+                        {
+                            "tool_call_id": id,
+                            "output": response,
+                        }
+                    )
+                else:
+                    raise RuntimeError(f"Unknown tool call: {name}")
+            run = await llm.beta.threads.runs.submit_tool_outputs(
+                thread_id=thread.id,
+                run_id=run.id,
+                tool_outputs=tool_outputs,
+            )
+        else:
+            time.sleep(1)
+    MESSAGES = await llm.beta.threads.messages.list(
+        thread_id=thread.id,
+    )
+    for message in MESSAGES.data:
+        for content in message.content:
+            print(content.text.value)
+    await llm.beta.assistants.delete(
+        assistant_id=assistant.id,
+    )
+    await llm.beta.threads.delete(
+        thread_id=thread.id,
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
