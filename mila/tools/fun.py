fbb2a79f2aeec4640f8ddb7c836601ca3b6a2dc5
Kristen Steffen <762141+haxys@users.noreply.github.com>
2023-11-22 22:35:57 -0600

    Switch to using asyncio. This allows for proper asynchronous requests.

--- mila/tools/fun.py
+++ mila/tools/fun.py
@@ -3,7 +3,7 @@
 import json
 import os
 
-import requests
+import aiohttp
 
 from mila.logging import LOGGER
 
@@ -14,8 +14,12 @@ async def get_horoscope(star_sign: str) -> str:
     path = "/api/v1/get-horoscope/daily"
     params = f"?sign={star_sign}&day=today"
     LOGGER.info("Function called: get_horoscope(star_sign='%s')", star_sign)
-    response = requests.get(base_url + path + params, timeout=5)
-    return json.dumps(response.json())
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            base_url + path + params, timeout=5
+        ) as response:
+            return await response.text()
 
 
 get_horoscope.properties = {
@@ -32,13 +36,14 @@ async def get_meme_templates() -> str:
     base_url = "https://api.imgflip.com"
     path = "/get_memes"
     LOGGER.info("Function called: get_meme_templates()")
-    response = requests.get(base_url + path, timeout=5)
-    data = response.json()
-    # Select only 2-box memes.
-    memes = data["data"]["memes"]
-    memes = [meme for meme in memes if meme["box_count"] == 2]
-    data["data"]["memes"] = memes
-    return json.dumps(data)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(base_url + path, timeout=5) as response:
+            data = await response.json()
+            memes = data["data"]["memes"]
+            memes = [meme for meme in memes if meme["box_count"] == 2]
+            data["data"]["memes"] = memes
+            return json.dumps(data)
 
 
 get_meme_templates.properties = {}
@@ -47,7 +52,6 @@ get_meme_templates.required = []
 
 async def get_meme(template_id: int, text0: str, text1: str) -> str:
     """Get a meme from a template. Please make it funny."""
-    # Make a POST request to the Imgflip API.
     username = os.getenv("IMGFLIP_USERNAME")
     password = os.getenv("IMGFLIP_PASSWORD")
     base_url = "https://api.imgflip.com"
@@ -65,8 +69,12 @@ async def get_meme(template_id: int, text0: str, text1: str) -> str:
         text0,
         text1,
     )
-    response = requests.post(base_url + path, params=params, timeout=5)
-    return json.dumps(response.json())
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            base_url + path, data=params, timeout=5
+        ) as response:
+            return await response.text()
 
 
 get_meme.properties = {
