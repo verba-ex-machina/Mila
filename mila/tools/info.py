b861ffd02a47f21a9b43c32652129acd24ec4780
Kristen Steffen <762141+haxys@users.noreply.github.com>
2023-11-22 23:25:58 -0600

    Added SerpApi and the ability to search and scrape the web.

--- mila/tools/info.py
+++ mila/tools/info.py
@@ -4,25 +4,27 @@ import json
 import os
 
 import aiohttp
+import serpapi
 
 from mila.logging import LOGGER
 
 
 async def get_weather(zipcode: str) -> str:
     """Get the weather for a given location in the USA."""
-    apikey = os.getenv("OPENWEATHERMAP_API_KEY")
-    if not apikey:
+    api_key = os.getenv("OPENWEATHERMAP_API_KEY")
+    if not api_key:
+        err = (
+            "No OpenWeatherMap API key found. An API key can be obtained from"
+            " https://openweathermap.org/."
+        )
+        LOGGER.error(err)
         return json.dumps(
             {
-                "error": (
-                    "No OpenWeatherMap API key found. "
-                    + "An API key can be obtained from "
-                    + "https://openweathermap.org/."
-                )
+                "error": err,
             }
         )
     base_url = "https://api.openweathermap.org/data/2.5/forecast"
-    base_url += f"?zip={zipcode},us&appid={apikey}&units=imperial"
+    base_url += f"?zip={zipcode},us&appid={api_key}&units=imperial"
     LOGGER.info("Function called: get_weather(zipcode='%s')", zipcode)
 
     async with aiohttp.ClientSession() as session:
@@ -37,3 +39,55 @@ get_weather.properties = {
     }
 }
 get_weather.required = ["zipcode"]
+
+
+async def scrape_url(url: str) -> str:
+    """Scrape a given URL for its content. Scrapes raw HTML."""
+    LOGGER.info("Function called: scrape_url(url='%s')", url)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, timeout=5) as response:
+            return await response.text()
+
+
+scrape_url.properties = {
+    "url": {
+        "type": "string",
+        "description": "The URL to scrape.",
+    }
+}
+scrape_url.required = ["url"]
+
+
+async def search_duckduckgo(query: str) -> str:
+    """Search DuckDuckGo for sites related to a given query."""
+    api_key = os.getenv("SERPAPI_API_KEY")
+    if not api_key:
+        err = (
+            "No SerpAPI API key found. An API key can be obtained from"
+            " https://serpapi.com/."
+        )
+        LOGGER.error(err)
+        return json.dumps(
+            {
+                "error": err,
+            }
+        )
+    LOGGER.info("Function called: search_duckduckgo(query='%s')", query)
+    client = serpapi.GoogleSearch(
+        {
+            "engine": "duckduckgo",
+            "q": query,
+            "kl": "us-en",
+            "api_key": api_key,
+        }
+    )
+    return json.dumps(client.get_dict())
+
+
+search_duckduckgo.properties = {
+    "query": {
+        "type": "string",
+        "description": "The query to search for.",
+    }
+}
+search_duckduckgo.required = ["query"]
