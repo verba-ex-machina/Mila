9c1d51ff75b1afed913c3ee1ead6f9a25bf151b6
Kristen Steffen <762141+haxys@users.noreply.github.com>
2023-11-22 20:56:09 -0600

    Added meme generator.

--- mila/tools/fun.py
+++ mila/tools/fun.py
@@ -1,13 +1,14 @@
 """Provide a suite of fun tools."""
 
 import json
+import os
 
 import requests
 
 from mila.logging import LOGGER
 
 
-def get_horoscope(star_sign: str) -> str:
+async def get_horoscope(star_sign: str) -> str:
     """Get the horoscope for a given star sign."""
     base_url = "https://horoscope-app-api.vercel.app"
     path = "/api/v1/get-horoscope/daily"
@@ -24,3 +25,58 @@ get_horoscope.properties = {
     }
 }
 get_horoscope.required = ["star_sign"]
+
+
+async def get_meme_templates() -> str:
+    """Get available meme templates."""
+    base_url = "https://api.imgflip.com"
+    path = "/get_memes"
+    LOGGER.info("Function called: get_meme_templates()")
+    response = requests.get(base_url + path, timeout=5)
+    return json.dumps(response.json())
+
+
+get_meme_templates.properties = {}
+get_meme_templates.required = []
+
+
+async def get_meme(template_id: int, text0: str, text1: str) -> str:
+    """Get a meme from a template."""
+    username = os.getenv("IMGFLIP_USERNAME")
+    password = os.getenv("IMGFLIP_PASSWORD")
+    base_url = "https://api.imgflip.com"
+    path = "/caption_image"
+    params = "?" + "&".join(
+        [
+            f"template_id={template_id}",
+            f"username={username}",
+            f"password={password}",
+            f"text0={text0}",
+            f"text1={text1}",
+        ]
+    )
+    LOGGER.info(
+        "Function called: get_meme(template_id=%s, text0='%s', text1='%s')",
+        template_id,
+        text0,
+        text1,
+    )
+    response = requests.get(base_url + path + params, timeout=5)
+    return json.dumps(response.json())
+
+
+get_meme.properties = {
+    "template_id": {
+        "type": "integer",
+        "description": "The ID of the meme template.",
+    },
+    "text0": {
+        "type": "string",
+        "description": "The text for the top of the meme.",
+    },
+    "text1": {
+        "type": "string",
+        "description": "The text for the bottom of the meme.",
+    },
+}
+get_meme.required = ["template_id", "text0", "text1"]
--- mila/tools/util.py
+++ mila/tools/util.py
@@ -34,3 +34,48 @@ suggest_feature.properties = {
     },
 }
 suggest_feature.required = ["feature", "category", "implementation"]
+
+
+async def submit_bug(
+    bug: str,
+    category: str,
+    steps: str,
+    expected: str,
+    actual: str,
+) -> str:
+    """Submit a bug report to improve Mila's performance."""
+    LOGGER.info("Function called: submit_bug")
+    LOGGER.info("  - Bug: %s", bug)
+    LOGGER.info("  - Category: %s", category)
+    LOGGER.info("  - Steps: %s", steps)
+    LOGGER.info("  - Expected: %s", expected)
+    LOGGER.info("  - Actual: %s", actual)
+    return (
+        f"Your report for `{bug}` has been recorded. "
+        + "We appreciate your feedback!"
+    )
+
+
+submit_bug.properties = {
+    "bug": {
+        "type": "string",
+        "description": "The function name for the reported bug.",
+    },
+    "category": {
+        "type": "string",
+        "description": "A one-word category for the bug.",
+    },
+    "steps": {
+        "type": "string",
+        "description": "The steps to reproduce the bug.",
+    },
+    "expected": {
+        "type": "string",
+        "description": "The expected behavior.",
+    },
+    "actual": {
+        "type": "string",
+        "description": "The actual behavior.",
+    },
+}
+submit_bug.required = ["bug", "category", "steps", "expected", "actual"]
