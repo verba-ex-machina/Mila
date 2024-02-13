7da74ccee837aa8aeab63d678b4fa721260b7229
Kristen Steffen <762141+haxys@users.noreply.github.com>
2024-02-13 13:21:55 -0600

    Lint.

--- mila/module/discord/__init__.py
+++ mila/module/discord/__init__.py
@@ -5,17 +5,15 @@ from typing import Union
 from mila.base.interfaces import TaskIO
 from mila.base.types import MilaTask
 
+
 class DiscordIO(TaskIO):
     """Implement a Discord TaskIO adapter."""
 
     def __init__(self) -> None:
         """Initialize the DiscordIO."""
-        pass
 
     async def recv(self) -> Union[MilaTask, None]:
         """Receive a task from Discord."""
-        pass
 
     async def send(self, task: MilaTask) -> None:
         """Send a task to Discord."""
-        pass
