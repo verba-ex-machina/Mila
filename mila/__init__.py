d94a84817b433bc7b1b11feb7625944b7c741b1d
Kristen Steffen <762141+haxys@users.noreply.github.com>
2024-02-17 12:56:26 -0600

    Expanded MilaIO into its own echo server.

--- mila/__init__.py
+++ mila/__init__.py
@@ -1,9 +1,30 @@
 """Provide the Mila framework."""
 
-from .module.fake import FakeIO
+from typing import List
+
+from .base.interfaces import TaskIO
+from .base.types import MilaTask
 
 
-class MilaIO(FakeIO):
+class MilaIO(TaskIO):
     """Define the Mila TaskIO handler."""
 
-    # For now we're using the fake as an echo server.
+    tasks: List[MilaTask] = []
+
+    async def recv(self) -> List[MilaTask]:
+        """Receive a task from the Mila framework."""
+        tasks = self.tasks.copy()
+        self.tasks.clear()
+        return tasks
+
+    async def send(self, task: MilaTask) -> None:
+        """Send a task to the Mila framework."""
+        self.tasks.append(task)
+
+    def setup(self) -> None:
+        """Prepare the Mila IO channel."""
+        self.tasks.clear()
+
+    def teardown(self) -> None:
+        """Teardown the Mila IO channel."""
+        self.tasks.clear()
