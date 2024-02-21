2ff67be3ee48e446af21913d7324167f77805400
Kristen Steffen <762141+haxys@users.noreply.github.com>
2024-02-20 21:40:17 -0600

    Simplify?

--- mila/module/discord/__main__.py
+++ mila/module/discord/__main__.py
@@ -16,8 +16,7 @@ async def demo():
                 tasks = await discord.recv()
                 if any(task.content == "exit" for task in tasks):
                     running = False
-                else:
-                    await fakeio.send(tasks)
+                await fakeio.send(tasks)
                 await discord.send(await fakeio.recv())
                 await asyncio.sleep(0.1)
 
