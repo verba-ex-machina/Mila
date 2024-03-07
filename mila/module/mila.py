5b6e346880d43f81657c11370ff3f1d2740370b9
Kristen Steffen <762141+haxys@users.noreply.github.com>
2024-03-07 09:54:57 -0600

    WIP: Adding MilaStorage.

--- mila/module/mila.py
+++ mila/module/mila.py
@@ -3,15 +3,34 @@
 from typing import List
 
 from mila.base.constants import STATES
-from mila.base.interfaces import TaskIO
+from mila.base.interfaces import TaskIO, TaskStorage
 from mila.base.types import MilaTask
-from mila.module.fake import FakeStorage
+
+
+class MilaStorage(TaskStorage):
+    """Mila Framework storage class."""
+
+    async def create(self, task: MilaTask) -> str:
+        """Create a task in the storage channel."""
+
+    async def read(self, task_id: str) -> MilaTask:
+        """Read a task from the storage channel."""
+
+    async def update(self, task_id: str, task: MilaTask) -> None:
+        """Update a task in the storage channel."""
+
+    async def delete(self, task_id: str) -> None:
+        """Delete a task from the storage channel."""
+
+    async def by_state(self, state: str) -> List[MilaTask]:
+        """Retrieve tasks by state from the storage channel."""
+        return []
 
 
 class MilaIO(TaskIO):
     """Mila Framework I/O handler class."""
 
-    task_storage = FakeStorage()  # Replace with real storage.
+    task_storage: MilaStorage
 
     async def recv(self) -> List[MilaTask]:
         """Receive tasks from the I/O handler."""
@@ -21,3 +40,7 @@ class MilaIO(TaskIO):
         """Send tasks to the I/O handler."""
         for task in task_list:
             await self.task_storage.create(task)
+
+    async def setup(self) -> None:
+        """Prepare the I/O handler."""
+        self.task_storage = MilaStorage()
