6c0028cf7c85f499884bbf493fbac97e77a0c445
Kristen Steffen <762141+haxys@users.noreply.github.com>
2024-03-14 01:12:59 -0500

    Expanded type tests.

--- tests/base/test_types.py
+++ tests/base/test_types.py
@@ -1,5 +1,9 @@
 """Test Mila types."""
 
+import json
+from typing import Optional
+
+from mila.base.types import AssistantDefinition, MilaTool
 from tests.common import make_task
 
 
@@ -11,3 +15,76 @@ def test_mila_task():
     assert task == task2
     task2.dst.handler = "data2"
     assert task != task2
+
+
+def test_mila_tool():
+    """Test the MilaTool class."""
+
+    def test_function(a: str = "test", b: Optional[int] = None) -> bool:
+        """A function which always returns true."""
+        return a or b or 1
+
+    tool = MilaTool(
+        name="test tool",
+        function=test_function,
+        properties={
+            "a": {
+                "type": "string",
+                "description": "a test string",
+            },
+            "b": {
+                "type": "integer",
+                "description": "a test integer",
+            },
+        },
+        required=["a"],
+    )
+    assert tool.definition == {
+        "type": "function",
+        "function": {
+            "name": "test tool",
+            "description": "A function which always returns true.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "a": {
+                        "type": "string",
+                        "description": "a test string",
+                    },
+                    "b": {
+                        "type": "integer",
+                        "description": "a test integer",
+                    },
+                },
+                "required": ["a"],
+            },
+        },
+    }
+
+
+def test_assistant_definition():
+    """Test the AssistantDefinition class."""
+
+    def test_function() -> bool:
+        """A function which always returns true."""
+        return True
+
+    tool = MilaTool(
+        name="test tool",
+        function=test_function,
+        properties={},
+        required=[],
+    )
+    definition = {
+        "name": "test",
+        "description": "a test assistant",
+        "instructions": "do nothing",
+        "tools": [tool],
+        "model": "gpt-3.5-turbo",
+        "metadata": {
+            "test": "test",
+        },
+    }
+    assistant = AssistantDefinition(**definition)
+    definition["tools"] = [tool.definition for tool in assistant.tools]
+    assert str(assistant) == json.dumps(definition)
