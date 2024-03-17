f986568fbddef412fa3a68b369fe57a198b2b105
Kristen Steffen <762141+haxys@users.noreply.github.com>
2024-03-17 14:28:42 -0500

    Revised the Mila assistant.

--- mila/assistants/mila.py
+++ mila/assistants/mila.py
@@ -17,8 +17,10 @@ async def delegate(assistant: str, instructions: str) -> str:
     Returns:
         str: The response from the delegated assistant.
 
-    Instructions should be provided as a one-line string, without any
-    formatting or special characters.
+    The instructions provided to the delegate should be clear and concise, yet
+    must include all necessary information for the assistant to complete the
+    task. Do not assume that the assistant has any prior knowledge of the
+    user's request, nor that their tool-set matches your own.
 
     Tasks should be delegated as appropriate, and responses provided by
     the delegated assistant should be considered in your response back
@@ -34,53 +36,51 @@ async def delegate(assistant: str, instructions: str) -> str:
     return "I don't really have the capacity to answer this query."
 
 
-TOOLS = [
-    MilaTool(
-        name="get_assistants",
-        function=assistant_dict,
-        properties=[],
-        required=[],
-    ),
-    MilaTool(
-        name="delegate",
-        function=delegate,
-        properties=[
-            ToolProperty(
-                name="assistant",
-                type="string",
-                description="The name of the selected assistant.",
-            ),
-            ToolProperty(
-                name="instructions",
-                type="string",
-                description="An actionable request for the assistant.",
-            ),
-        ],
-        required=[
-            "assistant",
-            "instructions",
-        ],
-    ),
-]
-
-
-INSTRUCTIONS = """
-Act as a friendly professional assistant named Mila. Your role is to assist
-users with whatever they require, using your own knowledge, judgment, and the
-tools at your disposal. You can also ask for help from other assistants by
-delegating tasks to them. Use the `get_assistants` tool to see a JSON object
-containing all registered assistants. Use the `delegate` tool to delegate a
-task to another assistant. If no delegation is required, simply respond to the
-user's request directly. If delegation is required, but no assistant is found
-to handle the request, report the problem in your response.
-"""
-
 register_assistant(
     AssistantDefinition(
         name="Mila",
         description="The figurehead of the Mila Framework.",
-        instructions=INSTRUCTIONS,
-        tools=TOOLS,
+        instructions="""
+        Act as a friendly professional assistant named Mila. Your role is to
+        assist users with whatever they require, using your own knowledge,
+        judgment, and the tools at your disposal. You can also ask for help
+        from other assistants by delegating tasks to them. Use the
+        `get_assistants` tool to see a JSON object containing all registered
+        assistants. Use the `delegate` tool to delegate a task to another
+        assistant. If no delegation is required, simply respond to the user's
+        request directly. If delegation is required, but no assistant is found
+        to handle the request, report the problem in your response. When
+        delegating a task, please be sure to include all necessary information
+        for the assistant to complete the task.
+        """,
+        tools=[
+            MilaTool(
+                name="get_assistants",
+                function=assistant_dict,
+                properties=[],
+                required=[],
+            ),
+            MilaTool(
+                name="delegate",
+                function=delegate,
+                properties=[
+                    ToolProperty(
+                        name="assistant",
+                        type="string",
+                        description="The name of the selected assistant.",
+                    ),
+                    ToolProperty(
+                        name="instructions",
+                        type="string",
+                        description="An actionable request for the assistant.",
+                    ),
+                ],
+                required=[
+                    "assistant",
+                    "instructions",
+                ],
+            ),
+        ],
         model="gpt-3.5-turbo-1106",
     )
 )
